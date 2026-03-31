4d343cbc019f0c9a8c49615cd7d0cbc942dbe2a8
factoreminv <soysal@stanford.edu>
2026-03-31 17:51:13 +0000

    Deploying to gh-pages from @ factoreminv/factoreminv.github.io@1498d6f31ebb0c58f73c7f10a139d4839a79cee6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -87,6 +87,9 @@ ninja.data = [{
             },},{id: "news-presented-the-monomial-paper-at-apsipa-2025",
           title: 'Presented the monomial paper at APSIPA 2025',
           description: "",
+          section: "News",},{id: "news-neuromorphic-studio-launced",
+          title: 'Neuromorphic Studio launced!',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
